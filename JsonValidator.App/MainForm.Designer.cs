4e46d12ebf07a59cce586fe9210233098e02dee7
Andrija Cacanovic <andrija.cacanovic@sipodsoftware.com>
2022-09-19 11:21:24 +0200

    init

---
+++ JsonValidator.App/MainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace JsonValidator.App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtJson = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtPaste = new System.Windows.Forms.Button();
+            this.txtClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rtxtResult = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtJson
+            // 
+            this.txtJson.AcceptsReturn = true;
+            this.txtJson.AcceptsTab = true;
+            this.txtJson.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtJson.Location = new System.Drawing.Point(0, 0);
+            this.txtJson.Multiline = true;
+            this.txtJson.Name = "txtJson";
+            this.txtJson.Size = new System.Drawing.Size(288, 428);
+            this.txtJson.TabIndex = 3;
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(3, 3);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 2;
+            this.btnValidate.Text = "V&alidate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.BtnValidateClick);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtOutput.Location = new System.Drawing.Point(0, 428);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(288, 257);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnValidate);
+            this.flowLayoutPanel1.Controls.Add(this.txtPaste);
+            this.flowLayoutPanel1.Controls.Add(this.txtClear);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(708, 28);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // txtPaste
+            // 
+            this.txtPaste.Location = new System.Drawing.Point(84, 3);
+            this.txtPaste.Name = "txtPaste";
+            this.txtPaste.Size = new System.Drawing.Size(75, 23);
+            this.txtPaste.TabIndex = 3;
+            this.txtPaste.Text = "Paste";
+            this.txtPaste.UseVisualStyleBackColor = true;
+            // 
+            // txtClear
+            // 
+            this.txtClear.Location = new System.Drawing.Point(165, 3);
+            this.txtClear.Name = "txtClear";
+            this.txtClear.Size = new System.Drawing.Size(75, 23);
+            this.txtClear.TabIndex = 4;
+            this.txtClear.Text = "Clear";
+            this.txtClear.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtJson);
+            this.panel1.Controls.Add(this.txtOutput);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 685);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rtxtResult);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(288, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(420, 685);
+            this.panel2.TabIndex = 9;
+            // 
+            // rtxtResult
+            // 
+            this.rtxtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtResult.Location = new System.Drawing.Point(0, 20);
+            this.rtxtResult.Name = "rtxtResult";
+            this.rtxtResult.ReadOnly = true;
+            this.rtxtResult.Size = new System.Drawing.Size(420, 665);
+            this.rtxtResult.TabIndex = 10;
+            this.rtxtResult.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(420, 20);
+            this.panel3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 713);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TextBox txtJson;
+        private Button btnValidate;
+        private TextBox txtOutput;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button txtPaste;
+        private Button txtClear;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label1;
+        private RichTextBox rtxtResult;
+    }
+}
---
+++ JsonValidator.App/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JsonValidator.App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnValidateClick(object sender, EventArgs e)
+        {
+            this.txtOutput.Text = "";
+            this.rtxtResult.Text = this.txtJson.Text;
+            this.label1.BackColor = this.BackColor;
+
+            ValidationInfo res = IsValidJson(this.txtJson.Text);
+
+            if (res.IsSuccessful)
+            {
+                this.label1.Text = "VALID!";
+                this.label1.ForeColor = Color.White;
+                this.label1.BackColor = Color.Green;
+            }
+            else
+            {
+                this.label1.Text = "INVALID!";
+                this.label1.ForeColor = Color.White;
+                this.label1.BackColor = Color.Red;
+            }
+
+            WriteOutput(res.Message);
+        }
+
+        private static ValidationInfo IsValidJson(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return ValidationInfo.Failed("Empty value");
+            }
+
+            if (!ValidateBasicStructure(input))
+            {
+                return ValidationInfo.Failed("JSON value does not start or ends with { or ]");
+            }
+
+            try
+            {
+                var tmpObj = JsonValue.Parse(input);
+                return ValidationInfo.Successful();
+            }
+            catch (FormatException fex)
+            {
+                return ValidationInfo.Failed(fex.Message);
+            }
+            catch (System.Text.Json.JsonException jsonex) //some other exception
+            {
+                return new ValidationInfo(jsonex.Message, jsonex.LineNumber, jsonex.BytePositionInLine, false);
+            }
+            catch (Exception ex) //some other exception
+            {
+                return ValidationInfo.Failed(ex.Message);
+            }
+        }
+
+        private static bool ValidateBasicStructure(string strInput)
+        {
+            strInput = strInput.Trim();
+            return (strInput.StartsWith("{") && strInput.EndsWith("}")) ||
+                (strInput.StartsWith("[") && strInput.EndsWith("]"));
+        }
+
+        private void WriteOutput(string output)
+        {
+            this.txtOutput.Text = this.txtOutput.Text.Insert(0, $"{output}{Environment.NewLine}");
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+
+    internal record ValidationInfo(
+            string Message,
+            long? Line,
+            long? ByteInLine,
+            bool IsSuccessful)
+    {
+        internal static ValidationInfo Successful()
+        {
+            return new ValidationInfo(string.Empty, null, null, true);
+        }
+
+        internal static ValidationInfo Failed(string errorMessage)
+        {
+            return new ValidationInfo(errorMessage, null, null, false);
+        }
+    }
+}
